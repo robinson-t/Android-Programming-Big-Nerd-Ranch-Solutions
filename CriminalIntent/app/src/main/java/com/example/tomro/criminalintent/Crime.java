a75790037ad4c6bf33a258a293a9ff517668d8c0
robinson-t <38432068+robinson-t@users.noreply.github.com>
2018-08-10 17:07:03 +0200

    began the CriminalIntent Exercise

---
+++ CriminalIntent/app/src/main/java/com/example/tomro/criminalintent/Crime.java
@@ -0,0 +1,49 @@
+package com.example.tomro.criminalintent;
+
+import java.util.UUID;
+import java.util.Date;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        this.mId = UUID.randomUUID();
+        this.mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+
+
+
+}
---
+++ CriminalIntent/app/src/main/java/com/example/tomro/criminalintent/CrimeLab.java
@@ -0,0 +1,43 @@
+package com.example.tomro.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    private CrimeLab (Context context) {
+        this.mCrimes = new ArrayList<Crime>();
+        for (int i = 0; i < 100; i++) {
+            Crime c = new Crime();
+            c.setTitle("Crime" + (i+1));
+            c.setSolved(i%2 ==0);
+            mCrimes.add(c);
+
+        }
+    }
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    public List<Crime> getCrimes() {
+        return this.mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime c : this.mCrimes) {
+            if (c.getId().equals(id)) {
+                return c;
+            }
+        }
+        return null;
+    }
+}
