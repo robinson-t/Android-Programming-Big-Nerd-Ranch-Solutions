377b83113ee12dc7da619f61abd0ac463e2d9914
robinson-t <38432068+robinson-t@users.noreply.github.com>
2018-08-13 12:03:38 +0200

    Added buttons to skip to ends of the ViewPager and set them to disable on reaching end, completed challenge exercise

--- CriminalIntent/app/src/main/java/com/example/tomro/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/com/example/tomro/criminalintent/CrimePagerActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,6 +22,9 @@ public class CrimePagerActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
 
+    private Button mBeginningButton;
+    private Button mEndButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +32,13 @@ public class CrimePagerActivity extends AppCompatActivity {
 
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        final FragmentManager fragmentManager = getSupportFragmentManager();
         UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
             public Fragment getItem(int position) {
                 Crime crime = mCrimes.get(position);
+                updateSkipButton();
                 return CrimeFragment.newInstance(crime.getId());
             }
 
@@ -46,6 +52,35 @@ public class CrimePagerActivity extends AppCompatActivity {
                 mViewPager.setCurrentItem(i);
                 break;
             }
+        }
+        mBeginningButton = (Button) findViewById(R.id.skip_beginning_button);
+        mEndButton = (Button) findViewById(R.id.skip_end_button);
+        mBeginningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0, false);
+            }
+        });
+        mEndButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(mCrimes.size()-1, false);
+            }
+        });
+
+    }
+
+    private void updateSkipButton() {
+        int index = mViewPager.getCurrentItem();
+        if (index == mCrimes.size()-1) {
+            mEndButton.setEnabled(false);
+            mBeginningButton.setEnabled(true);
+        } else if (index == 0) {
+            mBeginningButton.setEnabled(false);
+            mEndButton.setEnabled(true);
+        } else {
+            mBeginningButton.setEnabled(true);
+            mEndButton.setEnabled(true);
         }
     }
 
